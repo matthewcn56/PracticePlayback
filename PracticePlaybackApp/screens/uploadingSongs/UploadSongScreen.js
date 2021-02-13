516c1e72b0845175847302fff1fd2dbc95d24c30
matthewcn56 <65370631+matthewcn56@users.noreply.github.com>
2021-02-13 14:47:54 -0800

    Pre expo-update

--- PracticePlaybackApp/screens/uploadingSongs/UploadSongScreen.js
+++ PracticePlaybackApp/screens/uploadingSongs/UploadSongScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function UploadSongScreen(props) {
+  const {selectedImage} = props.route.params;
   return (
     <View style={styles.container}>
       <Text>This is our UploadSongScreen</Text>
-
+      {
+        selectedImage && 
+        <View>
+          <Image source = {{uri : selectedImage}} style = {styles.imageStyles}/>
+        </View>
+      }
       <Button
         onPress= {() => props.navigation.navigate("SelectImage")}
         title = "Select a Different Image"
@@ -21,4 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imageStyles: {
+    width: 300,
+    height: 200
+  }
 });
