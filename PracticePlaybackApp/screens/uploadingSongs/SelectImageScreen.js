66f98ee0ef1c6bf78d1411b6b3a296f8f3092957
Matthew Nieva <65370631+matthewcn56@users.noreply.github.com>
2021-02-13 16:23:41 -0800

    Uploading pictures functionality implemented (#1)
    * Pre expo-update
    
    * Uploading Song Picture functionality added

--- PracticePlaybackApp/screens/uploadingSongs/SelectImageScreen.js
+++ PracticePlaybackApp/screens/uploadingSongs/SelectImageScreen.js
@@ -1,16 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button, Image, Platform} from 'react-native';
+import * as ImagePicker from "expo-image-picker";
 
 export default function SelectImageScreen(props) {
+  const [selectedImage, setSelectedImage]= useState(null);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+    console.log("image selected is " +result.uri);
+    if (!result.cancelled){
+      setSelectedImage(result.uri);
+    }
+  }
+  //ask for camera roll permissions when screen gets mounted
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web'){
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted'){
+          alert("Camera roll permissions required to upload sheet music!");
+        }
+      }
+    })();
+  }, []);
+
   return (
     <View style={styles.container}>
       <Text>This is our SelectImageScreen</Text>
 
-      <Button
-        onPress= {() => props.navigation.navigate("UploadSong")}
-        title = "Upload A Song"
+    <Button
+        onPress= {pickImage}
+        title = { selectedImage ? "Select A Different Picture" : "Choose A Picture"}
       />
+      {
+        selectedImage && //conditionally render the image and the button to upload with the image
+        <View>
+          <Image source = {{uri : selectedImage}} style = {styles.imageStyles}/>
+          <Button
+          onPress= {() => props.navigation.navigate("UploadSong", {
+            selectedImage: selectedImage
+          })}
+          title = "Upload Song with Selected Picture"
+        />
+        </View>
+        
+
+      }
     </View>
+
+    
 
     
   );
@@ -23,4 +66,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  imageStyles: {
+    width: 300,
+    height: 200
+  }
 });
--- PracticePlaybackApp/screens/uploadingSongs/UploadSongScreen.js
+++ PracticePlaybackApp/screens/uploadingSongs/UploadSongScreen.js
@@ -1,24 +1,101 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, Image, TouchableOpacity } from 'react-native';
+import Slider from "@react-native-community/slider";
+import styles from "../../styles";
+import {Picker} from "@react-native-picker/picker";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function UploadSongScreen(props) {
+  const {selectedImage} = props.route.params;
+  const [tempo, setTempo] = useState(120);
+  const [instrument, setInstrument] = useState("Select Instrument");
+
+  function handleUploadSong(){
+    //test for invalid arguments
+    if(instrument ==="Select Instrument")
+      alert("Sheet music must have instrument associated with it!");
+  }
+
+  function decrementTempo() {
+    setTempo((prevValue) => {return (prevValue -1)})
+  }
+
+  function incrementTempo() {
+    setTempo((prevValue) => {return (prevValue+1)})
+  }
   return (
-    <View style={styles.container}>
-      <Text>This is our UploadSongScreen</Text>
+    <View style={pageStyles.screenContainer}>
+      <Text>Chosen Image</Text>
+        <View>
+          <Image source = {{uri : selectedImage}} style = {styles.imageStyles}/>
+        </View>
+        <Text>
+          Tempo: {tempo}
+        </Text>
+        <View style = {styles.spacedRow}>
+
+          <TouchableOpacity onPress={decrementTempo}>
+            <AntDesign name = "stepbackward" size = {24} color = "black"/>
+          </TouchableOpacity>
+
+          <Slider
+            style = {styles.slider}
+            minimumValue = {30}
+            step = {1}
+            value = {tempo}
+            maximumValue = {240}
+            onValueChange= {value => setTempo(value)}
+          />
+
+          <TouchableOpacity onPress = {incrementTempo} >
+            <AntDesign name = "stepforward" size = {24} color = "black"/>
+          </TouchableOpacity>
+
+          
+        </View>
+
+        <Text>
+          What Instrument Is This Sheet Music For?
+        </Text>
+         <Picker
+          selectedValue = {instrument}
+          style = {styles.picker}
+          onValueChange= {(itemValue, itemIndex) =>setInstrument(itemValue)}>
+            <Picker.Item label = "Select Instrument" value = "Select Instrument"/>
+            <Picker.Item label = "Clarinet" value = "clarinet"/>
+            <Picker.Item label = "Cello" value = "cello"/>
+            <Picker.Item label = "flute" value = "flute"/>
+
+          </Picker> 
+
+       
+
+        <Button
+        style = {pageStyles.uploadButton}
+        onPress= {handleUploadSong}
+        title = "Upload Song To Your Library"
+      />
 
       <Button
         onPress= {() => props.navigation.navigate("SelectImage")}
         title = "Select a Different Image"
       />
+
+     
+
+
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+const pageStyles = StyleSheet.create({
+  screenContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
   },
-});
+ 
+}
+
+)
